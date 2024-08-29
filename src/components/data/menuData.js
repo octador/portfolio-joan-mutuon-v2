a30bec61d46e5fdf0f802d48feeb2f4c0aed0925
Mutuon Joan <mutuon.j@gmail.com>
2024-08-29 13:06:03 +0200

    add component contact delet picture

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -2,5 +2,6 @@ export const mainMenu = [
      {link:"#", content:"accueil", icon:"lucide:home"},
      {link:"#about", content:"A propos", icon:"octicon:person-24"},
      {link:"#portafolio", content:"Projets", icon:"carbon:portfolio"},
+     {link:"#contact", content : "Me contacter", icon:"carbon:mail"}
 ]
 
